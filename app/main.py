798a634a823dba6ea33d7225cff07d22cde0e7a0
billyyfalcao <billycurso@gmail.com>
2026-09-01 20:14:32 -0300

    att bookstore

--- app/main.py
+++ app/main.py
@@ -2,10 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
+livros = ["noites brancas", "a metamorfose", "crime e castigo"]
+
 @app.get("/")
 async def home():
-    return {"message": "abobora"}
+    return {"message": "bem vindo a bookstore!"}
 
-@app.get ("/saudacao")
-async def saudar(nome:str):
-    return {"message": f"salve, {nome} seja bem vindo!" }
